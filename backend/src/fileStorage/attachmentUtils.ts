a186b10125876c98391544560bad98276fdc9e4c
Nguyễn Thắng <sor4@MacBook-Pro-cua-NguyenThang.local>
2022-08-19 00:26:37 +0700

    Added download and delete attachment function

--- backend/src/fileStorage/attachmentUtils.ts
+++ backend/src/fileStorage/attachmentUtils.ts
@@ -6,14 +6,14 @@ const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('helpers-attachmentUtils')
 
- // ##TODO: Implement the fileStorage logic
+// ##TODO: Implement the fileStorage logic
 export class AttachmentUtils {
     constructor(
-      private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
-      private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
-      private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
-    ) {}
-  
+        private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+        private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
+        private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
+    ) { }
+
     async createAttachmentPresignedUrl(attachmentId: string): Promise<string> {
         logger.info("Creating presigned url")
         return this.s3.getSignedUrl('putObject', {
@@ -26,5 +26,26 @@ export class AttachmentUtils {
     async getAttachmentUrl(attachmentId: string): Promise<string> {
         const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${attachmentId}`
         return attachmentUrl
+    }
+
+    async getAttachmentDownloadLink(attachmentId: string): Promise<string> {
+        return this.s3.getSignedUrl('getObject', {
+            Bucket: this.bucketName,
+            Key: attachmentId,
+            Expires: parseInt(this.urlExpiration)
+        })
+    }
+
+    async deleteAttachment(attachmentId: string): Promise<void> {
+        logger.info("attachmentId " + JSON.stringify(attachmentId))
+        try{
+            return this.s3.deleteObject( {
+                Bucket: this.bucketName,
+                Key: attachmentId,
+            }).promise()
+        }
+        catch(err){
+            logger.error("Error occurs: " + err)
+        }
     }
 }
